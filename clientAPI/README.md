0700b15686ac33787cf39302a910a731a825e9fc
minh <88233376+LeMinh2021@users.noreply.github.com>
2021-10-31 07:39:11 +0700

    API set up for ticket and user router

---
+++ clientAPI/README.md
@@ -0,0 +1,1 @@
+# Bus ticket side API 
--- clientAPI/app.js
+++ clientAPI/app.js
@@ -1,10 +1,47 @@
 const express = require("express")
 const app = express()
-
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
 const port = process.env.PORT || 3001
 
-app.use('/', (req,res,) => {res.json({message: "  anh em toi "}); });
+//API security 
+app.use(helmet());
+
+//handle CORS error
+app.use(cors());
+
+//Logger
+app.use(morgan("tiny"));
+
+//Set body bodyParser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//Load routers
+const userRouter = require("./src/routers/user.route");
+const ticketRouter = require("./src/routers/ticket.route");
+
+//use Routers
+app.use("/v1/user", userRouter)
+app.use("/v1/ticket", ticketRouter)
+
+//Error handler
+const handleError = require("./src/utils/errorHandler");
+
+app.use((req, res, next) => {
+  const error = new Error("Resources not found!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  handleError(error, res);
+});
+
+
 
 app.listen(port, () => {
-    console.log(`API is ready on http://localhost:${port}`)
-})
+    console.log(`API is ready on http://localhost:${port}`);
+});
